b3c2a3959ad0c329d796d09e0d085ecc8d6763c3
moshang <moshang>
2017-08-02 23:47:24 +0800

    首页完成

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,7 +5,7 @@ Page({
     navbar: ['头条', '新闻', '财经', '体育','军事','教育','科技'],
     currentTabIte: '头条',
     currentTabIdx:0,
-    
+    articleLists:[],
   },
   navbarTap: function (e) {
     console.log(e.currentTarget.dataset.ite);
@@ -13,18 +13,21 @@ Page({
       currentTabIdx: e.currentTarget.dataset.idx,
       currentTabIte: e.currentTarget.dataset.ite
     });
-    
+    var _this = this;
     wx.request({
       url: 'https://way.jd.com/jisuapi/get',
       data: {
-        channel: '头条',
+        channel: this.data.currentTabIte,
         appkey: 'fc4a06a254b6d27a0a94f0da15823181'
       },
       success: function (res) {
-        console.log(res);
+        console.log(res.data.result.result.list);
+        console.log(_this);
         if (res.statusCode == 200)
         {
-
+          _this.setData({
+            articleLists:res.data.result.result.list
+          })
         }
       }
     })
