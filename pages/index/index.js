e3b31152ac9c6fa13d3d08c3cebfe8b63b975377
moshang <moshang>
2017-08-15 23:41:20 +0800

    v0.1

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,48 +4,47 @@ Page({
   data: {
     navbar: ['头条', '新闻', '财经', '体育', '军事', '教育', '科技','NBA','股票','星座','女性','健康','育儿'],
     currentTabIte: '头条',
+    url: 'https://way.jd.com/jisuapi/get',
+    appkey: 'fc4a06a254b6d27a0a94f0da15823181',
     currentTabIdx:0,
     articleLists:[],
-    isHideLoadMore:false,
     start:0,
     num:10,
     animationData: {}
   },
-  getNews:function(start,num){
+  getNews:function(start,num,type=1){
     var that = this;
     wx.request({
-      url: 'https://way.jd.com/jisuapi/get',
+      url: that.data.url,
       data: {
         channel: that.data.currentTabIte,
-        appkey: 'fc4a06a254b6d27a0a94f0da15823181',
+        appkey: that.data.appkey,
         start: start,
         num: num
       },
       success: function (res) {
         wx.hideNavigationBarLoading()
         if (res.statusCode == 200) {
-          that.showTip();
-          that.setData({
-            articleLists: res.data.result.result.list.concat(that.data.articleLists),
-            // isHideLoadMore: true
-          });
-          
+          var returnData = res.data.result.result.list;
+          if(type==1){
+            that.setData({
+              articleLists: returnData.concat(that.data.articleLists),
+            });
+            that.showTip();
+          }else{
+            that.setData({
+              articleLists: that.data.articleLists.concat(returnData),
+            });
+          }
           // 缓存
           wx.setStorage({
             key: "key",
             data: that.data.articleLists,
             success: function () {
-              console.log('strorage');
+              // console.log('strorage');
             }
           });
           wx.stopPullDownRefresh();
-          
-          // setTimeout(function(){
-          //   that.setData({
-          //     isHideLoadMore:false
-          //   })
-          // },1500);
-          that.hideTip();
         }
       }
     });
@@ -84,50 +83,31 @@ Page({
   // xiahua
   onReachBottom: function () {
     this.setData({
-      isHideLoadMore:true
+      start: this.data.start + 10
     });
-    // this.setData({
-    //   start: this.data.start+10
-    // });
-    // this.getNews(this.data.start,10);
-    this.setData({
-      isHideLoadMore:false
-    });
+    this.getNews(this.data.start, 10,2);
   },
 
-  // anot
-  hideTip: function () {
+  // animation
+  showTip: function () {
     var animation = wx.createAnimation({
-      duration: 2500,
+      duration: 1000,
       timingFunction: 'ease',
     })
 
     this.animation = animation
-    animation.height(0).opacity(0).step()
+    animation.height(36).width('100%').opacity(1).step()
     
     this.setData({
       animationData: animation.export()
     })
 
     setTimeout(function () {
+      animation.height(0).width('80%').opacity(0).step()
       this.setData({
-        isHideLoadMore: true
+        animationData: animation.export()
       })
-    }.bind(this), 1000);
+    }.bind(this), 2000);
     
-  },
-  showTip:function(){
-    var animation = wx.createAnimation({
-      duration: 100,
-      timingFunction: 'ease',
-    })
-    this.animation = animation
-    this.setData({
-      isHideLoadMore: false
-    })
-    animation.height(30).opacity(1).step();
-    this.setData({
-      animationData: animation.export()
-    })
   }
 }) 
